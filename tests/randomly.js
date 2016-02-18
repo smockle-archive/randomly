333161cee9bb587ae1e25eff541eaf055cf58734
Clay Miller <clay@smockle.com>
2016-02-18 09:41:53 -0800

    Add tests for Randomly#collection

--- tests/randomly.js
+++ tests/randomly.js
@@ -8,35 +8,29 @@ test('Randomly#string', (t) => {
     Randomly.string instanceof Function,
     'exists'
   )
-
   t.throws(
     Randomly.string.bind(this, 'length'),
     'throws an error if length is NaN'
   )
-
   t.throws(
     Randomly.string.bind(this, -1),
     'throws an error if length is less than zero'
   )
-
   t.equals(
     Randomly.string(0),
     '',
     'returns a zero-length string'
   )
-
   t.equals(
     Randomly.string(5).length,
     5,
     'returns a string of length 5'
   )
-
   t.equals(
     typeof Randomly.string(5),
     'string',
     'returns a string'
   )
-
   t.ok(
     /[a-zA-Z0-9]/.test(Randomly.string(5)),
     'returns a string of lowercase letters, uppercase letters and numbers'
@@ -50,52 +44,42 @@ test('Randomly#int', (t) => {
     Randomly.int instanceof Function,
     'exists'
   )
-
   t.throws(
     Randomly.int.bind(this, 'min', 1),
     'throws an error if min is NaN'
   )
-
   t.throws(
     Randomly.int.bind(this, 1, 'max'),
     'throws an error if max is NaN'
   )
-
   t.ok(
     Randomly.int(-1, -1000) < -1,
     'returns a random negative number below max'
   )
-
   t.ok(
     Randomly.int(-1, -1000) > -1000,
     'returns a random negative number above min'
   )
-
   t.ok(
     Randomly.int(1000, -1000) < 1000,
     'returns a random number below max'
   )
-
   t.ok(
     Randomly.int(1000, -1000) > -1000,
     'returns a random number above min'
   )
-
   t.ok(
     Randomly.int(1000, 1) < 1000,
     'returns a random positive number below max'
   )
-
   t.ok(
     Randomly.int(1000, 1) > 1,
     'returns a random positive number above min'
   )
-
   t.ok(
     Randomly.int(0, 1000) < 1000,
     'returns a random positive number below min (inverted min & max)'
   )
-
   t.ok(
     Randomly.int(0, 1000) > 0,
     'returns a random positive number above max (inverted min & max)'
@@ -109,23 +93,19 @@ test('Randomly#getLowerInt', (t) => {
     Randomly.getLowerInt instanceof Function,
     'exists'
   )
-
   t.throws(
     Randomly.getLowerInt.bind(this, 'degree'),
     'throws an error if degree is NaN'
   )
-
   t.throws(
     Randomly.getLowerInt.bind(this, 0),
     'throws an error if degree is equal to zero'
   )
-
   t.equals(
     Randomly.getLowerInt(1),
     0,
     'returns the smallest one-digit number'
   )
-
   t.equals(
     Randomly.getLowerInt(5),
     10000,
@@ -140,23 +120,19 @@ test('Randomly#getUpperInt', (t) => {
     Randomly.getUpperInt instanceof Function,
     'exists'
   )
-
   t.throws(
     Randomly.getUpperInt.bind(this, 'degree'),
     'throws an error if degree is NaN'
   )
-
   t.throws(
     Randomly.getUpperInt.bind(this, 0),
     'throws an error if degree is equal to zero'
   )
-
   t.equals(
     Randomly.getUpperInt(1),
     9,
     'returns the largest one-digit number'
   )
-
   t.equals(
     Randomly.getUpperInt(5),
     99999,
@@ -171,95 +147,97 @@ test('Randomly#create', (t) => {
     Randomly.create instanceof Function,
     'exists'
   )
-
   t.throws(
     Randomly.create.bind(this, Number, '0'),
     'throws an error if length is NaN'
   )
-
   t.equals(
     Randomly.create(String, 5).length,
     5,
     'creates a string of length 5'
   )
-
   t.equals(
     typeof Randomly.create(String, 5),
     'string',
     'creates a string'
   )
-
   t.equals(
     Randomly.create(Number, 5).toString().length,
     5,
     'creates a number of length 5'
   )
-
   t.equals(
     typeof Randomly.create(Number, 5),
     'number',
     'creates a number'
   )
-
   t.equals(
     Randomly.create(Object, 5).key.length,
     5,
     'creates an object of length 5'
   )
-
   t.equals(
     typeof Randomly.create(Object, 5),
     'object',
     'creates an object'
   )
 })
-//
-// test('Randomly#collection', (t) => {
-//   t.plan(13)
-//   it('exists', () => {
-//     assert.typeOf(Randomly.collection, 'function')
-//   })
-//
-//   it('throws an error if quantity is NaN', () => {
-//     assert.throw(Randomly.collection.bind(this, Number, '0', 1))
-//   })
-//
-//   it('throws an error if length is NaN', () => {
-//     assert.throw(Randomly.collection.bind(this, Number, 1, '0'))
-//   })
-//
-//   it('throws an error if quantity is less than zero', () => {
-//     assert.throw(Randomly.collection.bind(this, Number, -1, 1))
-//   })
-//
-//   it('throws an error if length is zero', () => {
-//     assert.throw(Randomly.collection.bind(this, Number, 1, 0, true))
-//   })
-//
-//   it('creates an empty array', () => {
-//     let array = Randomly.collection(Number, 0, 1)
-//     assert.lengthOf(array, 0)
-//     assert.deepEqual(array, [])
-//   })
-//
-//   it('creates an array of strings', () => {
-//     let array = Randomly.collection(String, 5)
-//     assert.lengthOf(array, 5)
-//     array.forEach(s => assert.typeOf(s, 'string'))
-//   })
-//
-//   it('creates an array of numbers', () => {
-//     let array = Randomly.collection(Number, 5)
-//     assert.lengthOf(array, 5)
-//     array.forEach(s => assert.typeOf(s, 'number'))
-//   })
-//
-//   it('creates an array of objects', () => {
-//     let array = Randomly.collection(Object, 5)
-//     assert.lengthOf(array, 5)
-//     array.forEach(s => assert.typeOf(s, 'object'))
-//   })
-// })
+
+test('Randomly#collection', (t) => {
+  t.plan(25)
+
+  t.ok(
+    Randomly.collection instanceof Function,
+    'exists'
+  )
+  t.throws(
+    Randomly.collection.bind(this, Number, '0', 1),
+    'throws an error if quantity is NaN'
+  )
+  t.throws(
+    Randomly.collection.bind(this, Number, 1, '0'),
+    'throws an error if length is NaN'
+  )
+  t.throws(
+    Randomly.collection.bind(this, Number, -1, 1),
+    'throws an error if quantity is less than zero'
+  )
+  t.throws(
+    Randomly.collection.bind(this, Number, 1, 0, true),
+    'throws an error if length is zero'
+  )
+  t.equals(
+    Randomly.collection(Number, 0, 1).length,
+    0,
+    'creates a zero-length array'
+  )
+  t.deepEquals(
+    Randomly.collection(Number, 0, 1),
+    [],
+    'creates an empty array'
+  )
+  t.equals(
+    Randomly.collection(String, 5).length,
+    5,
+    'creates an array of strings of length 5'
+  )
+  Randomly.collection(String, 5).forEach((x) =>
+    t.equals(typeof x, 'string', 'creates an array of strings'))
+  t.equals(
+    Randomly.collection(Number, 5).length,
+    5,
+    'creates an array of numbers of length 5'
+  )
+  Randomly.collection(Number, 5).forEach((x) =>
+    t.equals(typeof x, 'number', 'creates an array of numbers'))
+  t.equals(
+    Randomly.collection(Object, 5).length,
+    5,
+    'creates an array of objects of length 5'
+  )
+  Randomly.collection(Object, 5).forEach((x) =>
+    t.equals(typeof x, 'object', 'creates'))
+})
 //
 // test('Randomly#sort', (t) => {
 //   t.plan(8)
