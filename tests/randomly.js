ca45e9cab2cdd0f2757a7e9757e53d144651d749
Clay Miller <clay@smockle.com>
2016-02-17 22:28:51 -0800

    Add tests for Randomly#int

--- tests/randomly.js
+++ tests/randomly.js
@@ -3,132 +3,175 @@ const test = require('tape')
 
 test('Randomly#string', (t) => {
   t.plan(7)
-  t.ok(Randomly.string instanceof Function, 'exists')
-  t.throws(Randomly.string.bind(this, 'length'), 'throws an error if length is NaN')
-  t.throws(Randomly.string.bind(this, -1), 'throws an error if length is less than zero')
-  t.equals(Randomly.string(0), '', 'returns a zero-length string')
-  t.equals(Randomly.string(5).length, 5, 'returns a string of length 5')
-  t.equals(typeof Randomly.string(5), 'string', 'returns a string')
-  t.ok(/[a-zA-Z0-9]/.test(Randomly.string(5)), 'returns a string of lowercase letters, uppercase letters and numbers')
+
+  t.ok(
+    Randomly.string instanceof Function,
+    'exists'
+  )
+
+  t.throws(
+    Randomly.string.bind(this, 'length'),
+    'throws an error if length is NaN'
+  )
+
+  t.throws(
+    Randomly.string.bind(this, -1),
+    'throws an error if length is less than zero'
+  )
+
+  t.equals(
+    Randomly.string(0),
+    '',
+    'returns a zero-length string'
+  )
+
+  t.equals(
+    Randomly.string(5).length,
+    5,
+    'returns a string of length 5'
+  )
+
+  t.equals(
+    typeof Randomly.string(5),
+    'string',
+    'returns a string'
+  )
+
+  t.ok(
+    /[a-zA-Z0-9]/.test(Randomly.string(5)),
+    'returns a string of lowercase letters, uppercase letters and numbers'
+  )
 })
 
-// test('Randomly#int', (t) => {
-//   t.plan(11)
-//   it('exists', () => {
-//     assert.typeOf(Random.int, 'function')
-//   })
-//
-//   it('throws an error if min is NaN', () => {
-//     assert.throw(Random.int.bind(this, 'min', 1))
-//   })
-//
-//   it('throws an error if max is NaN', () => {
-//     assert.throw(Random.int.bind(this, 1, 'max'))
-//   })
-//
-//   it('returns a random negative number', () => {
-//     let min = -1000
-//     let max = -1
-//     let randomNumber = Random.int(min, max)
-//     assert.isBelow(randomNumber, max)
-//     assert.isAbove(randomNumber, min)
-//   })
-//
-//   it('returns a random number', () => {
-//     let min = -1000
-//     let max = 1000
-//     let randomNumber = Random.int(min, max)
-//     assert.isBelow(randomNumber, max)
-//     assert.isAbove(randomNumber, min)
-//   })
-//
-//   it('returns a positive number', () => {
-//     let min = 1
-//     let max = 1000
-//     let randomNumber = Random.int(min, max)
-//     assert.isBelow(randomNumber, max)
-//     assert.isAbove(randomNumber, min)
-//   })
-//
-//   it('returns a positive number (inverted min & max)', () => {
-//     let min = 1000
-//     let max = 0
-//     let randomNumber = Random.int(min, max)
-//     assert.isBelow(randomNumber, min)
-//     assert.isAbove(randomNumber, max)
-//   })
-// })
+test('Randomly#int', (t) => {
+  t.plan(11)
+
+  t.ok(
+    Randomly.int instanceof Function,
+    'exists'
+  )
+
+  t.throws(
+    Randomly.int.bind(this, 'min', 1),
+    'throws an error if min is NaN'
+  )
+
+  t.throws(
+    Randomly.int.bind(this, 1, 'max'),
+    'throws an error if max is NaN'
+  )
+
+  t.ok(
+    Randomly.int(-1, -1000) < -1,
+    'returns a random negative number below max'
+  )
+
+  t.ok(
+    Randomly.int(-1, -1000) > -1000,
+    'returns a random negative number above min'
+  )
+
+  t.ok(
+    Randomly.int(1000, -1000) < 1000,
+    'returns a random number below max'
+  )
+
+  t.ok(
+    Randomly.int(1000, -1000) > -1000,
+    'returns a random number above min'
+  )
+
+  t.ok(
+    Randomly.int(1000, 1) < 1000,
+    'returns a random positive number below max'
+  )
+
+  t.ok(
+    Randomly.int(1000, 1) > 1,
+    'returns a random positive number above min'
+  )
+
+  t.ok(
+    Randomly.int(0, 1000) < 1000,
+    'returns a random positive number below min (inverted min & max)'
+  )
+
+  t.ok(
+    Randomly.int(0, 1000) > 0,
+    'returns a random positive number above max (inverted min & max)'
+  )
+})
 //
 // test('Randomly#getLowerInt', (t) => {
 //   t.plan(5)
 //   it('exists', () => {
-//     assert.typeOf(Random.getLowerInt, 'function')
+//     assert.typeOf(Randomly.getLowerInt, 'function')
 //   })
 //
 //   it('throws an error if degree is NaN', () => {
-//     assert.throw(Random.getLowerInt.bind(this, 'degree'))
+//     assert.throw(Randomly.getLowerInt.bind(this, 'degree'))
 //   })
 //
 //   it('throws an error if degree is equal to zero', () => {
-//     assert.throw(Random.getLowerInt.bind(this, 0))
+//     assert.throw(Randomly.getLowerInt.bind(this, 0))
 //   })
 //
 //   it('returns the smallest one-digit number', () => {
-//     assert.strictEqual(Random.getLowerInt(1), 0)
+//     assert.strictEqual(Randomly.getLowerInt(1), 0)
 //   })
 //
 //   it('returns the smallest n-digit number', () => {
-//     assert.strictEqual(Random.getLowerInt(5), 10000)
+//     assert.strictEqual(Randomly.getLowerInt(5), 10000)
 //   })
 // })
 //
 // test('Randomly#getUpperInt', (t) => {
 //   t.plan(5)
 //   it('exists', () => {
-//     assert.typeOf(Random.getUpperInt, 'function')
+//     assert.typeOf(Randomly.getUpperInt, 'function')
 //   })
 //
 //   it('throws an error if degree is NaN', () => {
-//     assert.throw(Random.getUpperInt.bind(this, 'degree'))
+//     assert.throw(Randomly.getUpperInt.bind(this, 'degree'))
 //   })
 //
 //   it('throws an error if degree is equal to zero', () => {
-//     assert.throw(Random.getUpperInt.bind(this, 0))
+//     assert.throw(Randomly.getUpperInt.bind(this, 0))
 //   })
 //
 //   it('returns the largest one-digit number', () => {
-//     assert.strictEqual(Random.getUpperInt(1), 9)
+//     assert.strictEqual(Randomly.getUpperInt(1), 9)
 //   })
 //
 //   it('returns the largest n-digit number', () => {
-//     assert.strictEqual(Random.getUpperInt(5), 99999)
+//     assert.strictEqual(Randomly.getUpperInt(5), 99999)
 //   })
 // })
 //
 // test('Randomly#create', (t) => {
 //   t.plan(8)
 //   it('exists', () => {
-//     assert.typeOf(Random.create, 'function')
+//     assert.typeOf(Randomly.create, 'function')
 //   })
 //
 //   it('throws an error if length is NaN', () => {
-//     assert.throw(Random.create.bind(this, Number, '0'))
+//     assert.throw(Randomly.create.bind(this, Number, '0'))
 //   })
 //
 //   it('creates a string', () => {
-//     let string = Random.create(String, 5)
+//     let string = Randomly.create(String, 5)
 //     assert.lengthOf(string, 5)
 //     assert.typeOf(string, 'string')
 //   })
 //
 //   it('creates a number', () => {
-//     let number = Random.create(Number, 5)
+//     let number = Randomly.create(Number, 5)
 //     assert.lengthOf(number.toString(), 5)
 //     assert.typeOf(number, 'number')
 //   })
 //
 //   it('creates an object', () => {
-//     let object = Random.create(Object, 5)
+//     let object = Randomly.create(Object, 5)
 //     assert.lengthOf(object.key, 5)
 //     assert.typeOf(object, 'object')
 //   })
@@ -137,45 +180,45 @@ test('Randomly#string', (t) => {
 // test('Randomly#collection', (t) => {
 //   t.plan(13)
 //   it('exists', () => {
-//     assert.typeOf(Random.collection, 'function')
+//     assert.typeOf(Randomly.collection, 'function')
 //   })
 //
 //   it('throws an error if quantity is NaN', () => {
-//     assert.throw(Random.collection.bind(this, Number, '0', 1))
+//     assert.throw(Randomly.collection.bind(this, Number, '0', 1))
 //   })
 //
 //   it('throws an error if length is NaN', () => {
-//     assert.throw(Random.collection.bind(this, Number, 1, '0'))
+//     assert.throw(Randomly.collection.bind(this, Number, 1, '0'))
 //   })
 //
 //   it('throws an error if quantity is less than zero', () => {
-//     assert.throw(Random.collection.bind(this, Number, -1, 1))
+//     assert.throw(Randomly.collection.bind(this, Number, -1, 1))
 //   })
 //
 //   it('throws an error if length is zero', () => {
-//     assert.throw(Random.collection.bind(this, Number, 1, 0, true))
+//     assert.throw(Randomly.collection.bind(this, Number, 1, 0, true))
 //   })
 //
 //   it('creates an empty array', () => {
-//     let array = Random.collection(Number, 0, 1)
+//     let array = Randomly.collection(Number, 0, 1)
 //     assert.lengthOf(array, 0)
 //     assert.deepEqual(array, [])
 //   })
 //
 //   it('creates an array of strings', () => {
-//     let array = Random.collection(String, 5)
+//     let array = Randomly.collection(String, 5)
 //     assert.lengthOf(array, 5)
 //     array.forEach(s => assert.typeOf(s, 'string'))
 //   })
 //
 //   it('creates an array of numbers', () => {
-//     let array = Random.collection(Number, 5)
+//     let array = Randomly.collection(Number, 5)
 //     assert.lengthOf(array, 5)
 //     array.forEach(s => assert.typeOf(s, 'number'))
 //   })
 //
 //   it('creates an array of objects', () => {
-//     let array = Random.collection(Object, 5)
+//     let array = Randomly.collection(Object, 5)
 //     assert.lengthOf(array, 5)
 //     array.forEach(s => assert.typeOf(s, 'object'))
 //   })
@@ -184,30 +227,30 @@ test('Randomly#string', (t) => {
 // test('Randomly#sort', (t) => {
 //   t.plan(8)
 //   it('exists', () => {
-//     assert.typeOf(Random.sort, 'function')
+//     assert.typeOf(Randomly.sort, 'function')
 //   })
 //
 //   it('sorts an empty array', () => {
-//     assert.deepEqual(Random.sort([]), [])
+//     assert.deepEqual(Randomly.sort([]), [])
 //   })
 //
 //   it('sorts an array of numbers', () => {
-//     let unsortedArray = Random.collection(Number, 100)
-//     let sortedArray = Random.sort(unsortedArray)
+//     let unsortedArray = Randomly.collection(Number, 100)
+//     let sortedArray = Randomly.sort(unsortedArray)
 //     assert.lengthOf(sortedArray, unsortedArray.length)
 //     assert.notDeepEqual(sortedArray, unsortedArray)
 //   })
 //
 //   it('sorts an array of strings', () => {
-//     let unsortedArray = Random.collection(String, 100)
-//     let sortedArray = Random.sort(unsortedArray)
+//     let unsortedArray = Randomly.collection(String, 100)
+//     let sortedArray = Randomly.sort(unsortedArray)
 //     assert.lengthOf(sortedArray, unsortedArray.length)
 //     assert.notDeepEqual(sortedArray, unsortedArray)
 //   })
 //
 //   it('sorts an array of objects', () => {
-//     let unsortedArray = Random.collection(Object, 100)
-//     let sortedArray = Random.sort(unsortedArray)
+//     let unsortedArray = Randomly.collection(Object, 100)
+//     let sortedArray = Randomly.sort(unsortedArray)
 //     assert.lengthOf(sortedArray, unsortedArray.length)
 //     assert.notDeepEqual(sortedArray, unsortedArray)
 //   })
